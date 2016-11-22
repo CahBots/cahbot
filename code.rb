c9555b5a1a5d53b60167b9e7c47370773244272c
2003cah <2003cah@gmail.com>
2016-11-22 15:55:19 -0500

    Added two new commands, Ping removed for now

--- code.rb
+++ code.rb
@@ -1,9 +1,13 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: 'MjQ5MjY4MzkzODE3OTMxNzc2.CxEv9Q.s8vb2OMalugoBiwAu2g3P21zmqA', client_id: 249268393817931776
+bot = Discordrb::Commands::CommandBot.new token: 'MjQ5MjY4MzkzODE3OTMxNzc2.CxPvrg.q-NUnZK5BDt1htnOyV__02XSReg', application_id: 249268393817931776, prefix: '^'
 
-bot.message(with_text: "Ping!") do |event|
-  event.respond "Pong!"
+bot.command :rnumber do |event, min, max|
+  rand(min.to_i .. max.to_i)
+end
+
+bot.command(:servreg, chain_usable: false, description: "Gets the region the server is stationed in.", permission_level: 1) do
+  event.server.region
 end
 
 bot.run
