5445d9d4598cc38957eb233425a2368a1c0378f1
2003cah <2003cah@gmail.com>
2016-11-22 21:36:36 -0500

    GRR

--- code.rb
+++ code.rb
@@ -1,11 +1,11 @@
 require 'discordrb'
 
-bot = Discordrb::Commands::CommandBot.new token: 'MjQ5MjY4MzkzODE3OTMxNzc2.CxPvrg.q-NUnZK5BDt1htnOyV__02XSReg', client_id: 249268393817931776, prefix: '^'
+bot = Discordrb::Commands::CommandBot.new token: 'MjQ5MjY4MzkzODE3OTMxNzc2.CxPvrg.q-NUnZK5BDt1htnOyV__02XSReg'  client_id: 249268393817931776  prefix: '^'
 
-bot.command(:shutdown, help_available: false) do |event|
+bot.command(:shutdown  help_available: false) do |event|
   break unless event.user.id == 228290433057292288
 
-  bot.send_message(event.channel.id, 'Cah has turned off Cah Bot')
+  bot.send_message(event.channel.id  'Cah has turned off Cah Bot')
   exit
 end
 
@@ -14,31 +14,31 @@ bot.command :ping do |event|
   m.edit "Pong! Time taken: #{Time.now - event.timestamp} seconds."
 end
 
-bot.command :rnumber do |event, min, max|
+bot.command :rnumber do |event  min  max|
   rand(min.to_i .. max.to_i)
 end
 
-bot.command(:servreg, chain_usable: false, description: "Gets the region the server is stationed in.") do
+bot.command(:servreg  chain_usable: false  description: "Gets the region the server is stationed in.") do
   event.server.region
 end
 
-bot.command(:invite, chain_usable: false) do |event|
+bot.command(:invite  chain_usable: false) do |event|
   event.bot.invite_url
 end
 
-bot.command(:donate, chain_usable: false) do |event|
-  bot.send_message(event.channel.id 'Donating? Click here, http://bit.ly/2gzJpnq'
+bot.command(:donate  chain_usable: false) do |event|
+  bot.send_message(event.channel.id 'Donating? Click here  http://bit.ly/2gzJpnq'
 end
 
-bot.command(:help, chain_usable: false) do |event|
-  event << ' Hello, welcome to the  help command , here are all commands'
-  event << ' ^servreg, Shows where the Discord server is stationed
-  event << ' ^rnumber, Gives you a random number. Syntax: ^rnumber "number" "other number"'
-  event << ' ^help, Shows this, duh'
-  event << ' ^ping, Used to show response time'
-  event << ' ^invite, Gives you a link to invite me'
-  event << ' ^shutdown, Shuts me down, only Cah can use this command'
-  event << ' ^donate, Want to donate? That's great! This command gives you a link for PayPal donations'
+bot.command(:help  chain_usable: false) do |event|
+  event << ' Hello and welcome to the  help command   here are all commands'
+  event << ' ^servreg Shows where the Discord server is stationed
+  event << ' ^rnumber Gives you a random number. Syntax: ^rnumber "number" "other number"'
+  event << ' ^help Shows this  duh'
+  event << ' ^ping Used to show response time'
+  event << ' ^invite Gives you a link to invite me'
+  event << ' ^shutdown Shuts me down  only Cah can use this command'
+  event << ' ^donate Want to donate? That's great! This command gives you a link for PayPal donations'
 end
 
 bot.run
