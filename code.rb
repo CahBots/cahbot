ba39a19c484597e65a7411372fd6094614b786ca
2003cah <2003cah@gmail.com>
2016-11-28 20:27:48 -0500

    Added the ^who command: Gives you a stupid story about CB

--- code.rb
+++ code.rb
@@ -24,7 +24,7 @@ bot.command :ping do |event|
   m.edit "Pong! Hey, that took: #{Time.now - event.timestamp} seconds to do."
 end
 
-bot.message(with_text: "^about") do |event|
+bot.commandd(:about, help_available: false) do |event|
   event.respond "Hello, I am Cah Bot, a small Discord bot with loads of potential. To learn more, or to tell me that I suck at bot creation, go here: https://discord.gg/zAbWXfe"
 end
 
@@ -39,11 +39,30 @@ end
 bot.command(:update, help_available: false) do |event|
   event << ' **Latest CahBot Update**'
   event << ''
-  event << 'Added the command you just used, updated the help, and I FINALLY know what I was doing wrong with commands, maybe...'
-  event << 'That\'s all for the first update (well, the first update that I put onto this command)'
+  event << 'Made sure all the commands were actual commands, and added the "^who" commands'
+  event << 'That\'s about all'
 end
 
-bot.message(with_text: "^donate") do |event|
+bot.command(:who, help_available: false) do |event|
+  event << 'Heyo, I\'m Cah ~~and this is my pawn shop~~ and I\'m the person who made CB.'
+  event << ''
+  event << 'I\'m 13 years old and my name is Christopher Holland (How\'s it going?).'
+  event << 'So you might be wondering why CB is a thing, well, let me tell you.'
+  event << ''
+  event << '**Once Upon A Time** there was a bored Cah wondering what to do with his time.'
+  event << 'He then wondered how hard it is to make a Discord bot. For some reason.'
+  event << 'Scavenging the internet he found...**discordrb**. It seemed like a good enough programming language to use.'
+  event << 'He then rushes to the Discord API server, the page to create a bot user, etc.'
+  event << 'It was at this moment that Cah realized, he had no idea how to code'
+  event << 'Through determination and running off of example commands for a short while, he made a fairly basic bot'
+  event << 'This bot, was Cah Bot.'
+  event << ''
+  event << 'But Cah wasn\'t satisfied enough, so he bought an Ubuntu (Through Digital Ocean) for $10 a month, to keep CB alive 24/7.'
+  event << 'And after nearly destroying it (thanks Discord API server for trying to destroy me), it worked!'
+  event << '***The End***'
+end
+
+bot.command(:donate, help_available: false) do |event|
   event.respond "Hi #{event.user.name}, click here for donations: http://bit.ly/2gmWLAx!"
 end
 
@@ -57,6 +76,7 @@ bot.command(:help, chain_usable: false) do |event|
   event << ' ^eval: Like you don\'t know what eval commands do'
   event << ' ^donate: Want to donate? That\'s great! This command gives you a link for PayPal donations'
   event << ' ^update: Gives you the latest CB update'
+  event << ' ^who: Gives you a stupid long story of how this came to be'
 end
 
 bot.run
