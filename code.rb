3f188820cd322cd112f736a646b559a9cb8e5f92
2003cah <2003cah@gmail.com>
2017-03-11 19:14:18 -0500

    aaaaaaa

--- code.rb
+++ code.rb
@@ -28,25 +28,73 @@ end
 bot.command(:eval, help_available: false) do |event, *code|
   if event.user.id == 228290433057292288
     begin
-      eval code.join(' ')
+      event << 'Eval Complete!'
+      event << ''
+      event << "Output: ```#{eval code.join(' ')} ```"
     rescue => e
-      event << "Ah geez, something went wrong, it says:"
-      event << "```"
-      event << "#{e} ```"
+      event << 'Eval Failed!'
+      event << ''
+      event << "Output: ```#{e} ```"
     end
   else
-     "Yo, you aren't allowed to do that!"
+    'Sorry, only Cah can eval stuff'
   end
 end
 
 bot.command(:restart, help_available: false) do |event|
   if event.user.id == 228290433057292288
     begin
-      event.respond ["Into the ***fuuuutttttuuuuurrrreeee***", "Please wait...", "How about n—", "Can do :thumbsup::skin-tone-1:", "Pong! Hey, that took... Oh wait, wrong command", "Ask again at a later ti—"].sample
-      exec("bash restart.sh")
+      event.respond ['Into the ***fuuuutttttuuuuurrrreeee***', 'Please wait...', 'How about n—', 'Can do :thumbsup::skin-tone-1:', 'Pong! Hey, that took... Oh wait, wrong command', 'Ask again at a later ti—'].sample
+      exec('bash restart.sh')
     end
   else
-    "Sorry, only Cah can update me"
+    'Sorry, only Cah can update me'
+  end
+end
+
+bot.command(:set, help_available: false) do |event, action, *args|
+  if event.user.id == 228290433057292288
+    case action
+    when 'avatar'
+      open(args.to_s) { |pic| event.bot.profile.avatar = pic }
+    when 'username'
+      name = args.to_s
+      event.respond "Username set to `#{bot.profile.username = name}`"
+    when 'game'
+      bot.game = args.to_s
+      event.respond 'GAME SET!'
+    when 'status'
+      online = bot.on
+      idle = bot.idle
+      invis = bot.invisible
+      dnd = bot.dnd
+      eval args.join; "Status Changed!"
+    else
+      'I don\'t know what to do!'
+    end
+  else
+    'Sorry, only Cah can set stuff'
+  end
+end
+
+bot.command(:ban, help_available: false, required_permissions: [:ban_members], permission_message: 'Heh, sorry, but you need the Ban Members permission to use this command', max_args: 1, min_args: 1, usage: 'B^ban <mention>') do |event, *args|
+  bot_profile = bot.profile.on(event.server)
+  can_do_the_magic_dance = bot_profile.permission?(:ban_members)
+  if can_do_the_magic_dance == true
+    if !event.message.mentions.empty?
+      begin
+        mention = bot.parse_mention("#{args.join}").id
+        event.server.ban("#{mention}", message_days = 7)
+        event.respond ["<@#{mention}> has been beaned, the past 7 days of messages from them have been deleted", "<@#{mention}> has been banned, the past 7 days of messages from them have been deleted"]
+      rescue => e
+        event.respond "The user you are trying to ban has a role higher than/equal to me. If you believe this is a mistake, report this to the CB Server"
+        bot.send_message(281280895577489409, "ERROR on server #{event.server.name} (ID: #{event.server.id}) for command `B^ban`, `#{e}`")
+      else
+        event.respond "Sorry, but I do not have the \"Ban Members\" permission"
+      end
+    else
+      event.respond "Sorry, but you need to mention the person you want to ban"
+    end
   end
 end
 
@@ -55,22 +103,22 @@ bot.command(:ping, help_available: false, max_args: 0) do |event|
   m.edit "Pong! Hey, that took #{((Time.now - event.timestamp) * 1000).to_i}ms."
 end
 
-bot.command([:eightball, :eball], help_available: false, min_args: 1) do |event|
+bot.command([:eightball, :eball, :'8ball'], help_available: false, min_args: 1) do |event|
   event.respond ["Sources say... Yeah", "Sources say... Nah", "Perhaps", "As I see it, yes", "As I see it, no", "If anything, probably", "Not possible", "Ask again at a later time", "Say that again?", "lol idk", "Probably not", "woahdude", "[object Object]", "Undoubtfully so", "I doubt it", "Eh, maybe"].sample
 end
 
 bot.command(:roll, help_available: false, max_args: 0) do |event|
-      h = event.respond '**Rolling Dice!**'
+  h = event.respond '**Rolling Dice!**'
   sleep 2
-      h.edit "And you got a... **#{rand(1..6)}!**"
+  h.edit "And you got a... **#{rand(1..6)}!**"
 end
 
 bot.command(:flip, help_available: false, max_args: 0) do |event|
-  m = event.respond ["woahdude, you got **Heads**", "woahdude, you got **Tails**", "You got **heads**", "You got **tails**"].sample
+  event.respond ["woahdude, you got **Heads**", "woahdude, you got **Tails**", "You got **heads**", "You got **tails**"].sample
 end
 
 bot.command(:flop, help_available: false, max_args: 0) do |event|
-  m = event.respond ["Oops, the coin flipped so high it didn't come back down", "The coin multiplied and landed on both", "The coin... disappeared", "Pong! It took **#{((Time.now - event.timestamp) * 1000).to_i}ms** to ping the coin", "And you got a... **#{rand(1..6)}!** wait thats not how coins work", "Perhaps you could resolve your situation without relying on luck", "noot", "[Witty joke concerning flipping a coin]", "[BOTTOM TEXT]"].sample
+  event.respond ["Oops, the coin flipped so high it didn't come back down", "The coin multiplied and landed on both", "The coin... disappeared", "Pong! It took **#{((Time.now - event.timestamp) * 1000).to_i}ms** to ping the coin", "And you got a... **#{rand(1..6)}!** wait thats not how coins work", "Perhaps you could resolve your situation without relying on luck", "noot", "[Witty joke concerning flipping a coin]", "[BOTTOM TEXT]"].sample
 end
 
 bot.command(:info, help_available: false, max_args: 0) do |event|
@@ -88,7 +136,7 @@ bot.message(with_text: 'CB prefix') do |event|
 end
 
 bot.command(:rnumber, help_available: false, min_args: 2, max_args: 2) do |event, min, max|
-  rand(min.to_i .. max.to_i)
+  rand(min.to_i..max.to_i)
 end
 
 bot.command(:invite, help_available: false, max_args: 0) do |event|
@@ -144,30 +192,42 @@ bot.command(:noot, help_available: false, max_args: 0) do |event|
 end
 
 bot.command([:cmds, :commands], chain_usable: false, max_args: 0) do |event|
-  event << ' Here are all of my commands for you to use!'
-  event << ' (upon saying "CB prefix") reminds you the prefix'
-  event << ' `^info`: Shows you some info about CB, or something'
-  event << ' `^rnumber <Number> <Other Number>`: Gives you a random number'
-  event << ' `^help`: Basically tells you to go here'
-  event << ' `^cmds`: pulls up this'
-  event << ' `^eightball`: Ask the 8ball something'
-  event << ' `^userinfo`: Shows some info about you'
-  event << ' `^reverse`: Reverses text'
-  event << ' `^flip`: Flips a coin, what else did you expect?'
-  event << ' `^flop`: Flops a coin, what expect did you else?'
-  event << ' `^ping`: Used to show response time'
-  event << ' `^servercount`: Returns the number of servers CB is in'
-  event << ' `^invite`: Gives you a link to invite me to your own server!'
-  event << ' `^die`: Shuts me down, only Cah can use this command'
-  event << ' `^roll`: Rolls a number between 1 and 6'
-  event << ' `^eval`: Like you don\'t know what eval commands do'
-  event << ' `^donate`: Want to donate? That\'s great! This command gives you a link for Patreon donations'
-  event << ' `^update`: Gives you the latest CB update'
-  event << ' `^say`: Makes CB say something, you need the manage messages perm tho'
-  event << ' `^thanks`: Thanks to these radical donors!'
-  event << ' `^feedback <words>`: Want to tell me my bot sucks without going into the CB Server? Well, here you go.'
-  event << ' `^noot`: noot'
+  event << 'Here are all of my commands for you to use!'
+  event << '*__Cah\'s Commands__*'
+  event << '`B^restart`: Pulls that fresh code and runs it'
+  event << '`B^die`: Kills the bot, without pulling any code or anything'
+  event << '`B^eval`: Like you don\'t know what eval commands do'
+  event << '`B^set <avatar|username|game|status> <args>`: Sets stuff'
+  event << ''
+  event << '*__Moderation Commands (in the works)__*'
+  event << '`B^ban <mention>`: Bans the user mentioned and deletes the past 7 days of messages from them'
+  event << '`B^say`: Makes CBB say something, you need the manage messages perm tho ~~yes I know it\'s not much of a moderation command shut up~~'
+  event << ''
+  event << '*__Fun Commands/Other Commands/Things I Was Too Lazy To Group__*'
+  event << '(upon saying "CBB prefix") reminds you the prefix'
+  event << '`B^info`: Shows you some info about CB, or something'
+  event << '`B^rnumber <Number> <Other Number>`: Gives you a random number'
+  event << '`B^help`: Basically tells you to go here'
+  event << '`B^cmds`: pulls up this'
+  event << '`B^eightball`: Ask the 8ball something'
+  event << '`B^userinfo`: Shows some info about you'
+  event << '`B^reverse`: Reverses text'
+  event << '`B^flip`: Flips a coin, what else did you expect?'
+  event << '`B^flop`: Flops a coin, what expect did you else?'
+  event << '`B^ping`: Used to show response time'
+  event << '`B^servercount`: Returns the number of servers CB is in'
+  event << '`B^invite`: Gives you a link to invite me to your own server!'
+  event << '`B^roll`: Rolls a number between 1 and 6'
+  event << '`B^donate`: Want to donate? That\'s great! This command gives you a link for Patreon donations'
+  event << '`B^update`: Gives you the latest CB update'
+  event << '`B^feedback <words>`: Sends your feedback to the CB Server'
+  event << '`B^thanks`: Thanks to these radical donors!'
+  event << '`B^trello`: The Trello board, woahdude'
+  event << '`B^noot`: noot (don\'t ask I didn\'t write this)'
+  event << ''
+  event << 'As always, if you find a horrible bug, report it in the CB Server <https://goo.gl/02ZRK5>'
 end
+
 
 bot.command(:feedback, min_args: 1) do |event, *args|
   if event.channel.pm? == true
