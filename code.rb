c31f935466f4cfdc0d281ecb7a0a79f988a9f34d
Advaith <advaith1@users.noreply.github.com>
2017-08-16 20:16:45 -0700

    update ^info
    make some cool changes
    im too bored to log them
    actually ynaught
    so
    here we go
    updated chews username, but still used the user username, not the bot username because i felt like it ¯\\\_(ツ)\_/¯
    changed the invite link to a discord.gg link bcuz discord.gg is k000000l
    realized that link went to #normal-chat so made it go to #info
    spammed `flop` in normal-chat cuz its k00l
    kthxbai

--- code.rb
+++ code.rb
@@ -137,9 +137,9 @@ bot.command(:info, help_available: false, max_args: 0) do |event|
   event << "***Info About CahBot:***"
   event << ""
   event << "**What is CahBot?** CB is a small Discord bot with loads of potential."
-  event << "**Who made CahBot?** Cah#5153 coded CahBot, with some help from happyzachariah#6121, tons of help from ChewLeKitten#6216 & others"
+  event << "**Who made CahBot?** Cah#5153 coded CahBot, with some help from happyzachariah#6121, tons of help from Chew#6216 & others"
   event << "**Why does CahBot exist?** One day I was bored so I made a Discord bot. End of story kthxbai"
-  event << "**Does CahBot have a server or something?** You bet, https://goo.gl/02ZRK5"
+  event << "**Does CahBot have a server or something?** You bet, https://discord.gg/cWmvfmz"
   event << "**u suk a bunnch an u can hardly mak a discord bawt.** Radical, thank you for noticing"
 end
 
