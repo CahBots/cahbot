390847b74608d3aaca5a5a26426d9009d1de6a75
Chewsterchew <chew@chew.pw>
2018-03-24 17:15:21 -0500

    Define bot as a constant

--- code.rb
+++ code.rb
@@ -3,35 +3,35 @@ require 'discordrb'
 require 'open-uri'
 require_relative 'config.rb'
 
-bot = Discordrb::Commands::CommandBot.new token: configatron.token, client_id: 249_268_393_817_931_776, prefix: ['<@249268393817931776> ', '^'], ignore_bots: true
+Bot = Discordrb::Commands::CommandBot.new token: configatron.token, client_id: 249_268_393_817_931_776, prefix: ['<@249268393817931776> ', '^'], ignore_bots: true
 
 Dir["#{File.dirname(__FILE__)}/modules/*.rb"].each { |file| require file }
 
 Modules = [Ban, CahBotServer, Commands, Die, Donate, EBall, Eval, Feedback, FlipFlop, Help, Info, Invite, Leave, Noot, Ping, Restart, Reverse, RNumber, Roll, Say, ServerCount, SetStuff, UserInfo].freeze
 
 Modules.each do |mod|
-  bot.include! mod
+  Bot.include! mod
   puts "Module #{mod} successfully loaded!"
 end
 
-bot.ready do |_event|
+Bot.ready do |_event|
   sleep 180
-  bot.game = 'Use ^cmds or ^info'
+  Bot.game = 'Use ^cmds or ^info'
   sleep 180
-  bot.game = "on #{bot.servers.count} servers!"
+  Bot.game = "on #{bot.servers.count} servers!"
   redo
 end
 
-bot.message(with_text: 'CB prefix') do |event|
+Bot.message(with_text: 'CB prefix') do |event|
   event.respond 'My prefix is `^`. For help, do `^help`'
 end
 
-bot.server_create do |event|
-  bot.send_message(281_280_895_577_489_409, "CahBot just joined `#{event.server.name}` (ID: #{event.server.id}), owned by `#{event.server.owner.distinct}` (ID: #{event.server.owner.id}), the server count is now #{bot.servers.count}")
+Bot.server_create do |event|
+  Bot.send_message(281_280_895_577_489_409, "CahBot just joined `#{event.server.name}` (ID: #{event.server.id}), owned by `#{event.server.owner.distinct}` (ID: #{event.server.owner.id}), the server count is now #{event.bot.servers.count}")
 end
 
-bot.server_delete do |event|
-  bot.send_message(281_280_895_577_489_409, "CahBot just left `#{event.server.name}` (ID: #{event.server.id}), the server count is now #{bot.servers.count}")
+Bot.server_delete do |event|
+  Bot.send_message(281_280_895_577_489_409, "CahBot just left `#{event.server.name}` (ID: #{event.server.id}), the server count is now #{event.bot.servers.count}")
 end
 
-bot.run
+Bot.run
