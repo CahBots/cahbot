016bdfd94a36153b41667f08925b989fce109067
Chewsterchew <chew@chew.pw>
2018-03-24 17:08:49 -0500

    Remove chew's other user id from restart

--- modules/restart.rb
+++ modules/restart.rb
@@ -2,7 +2,7 @@ module Restart
   extend Discordrb::Commands::CommandContainer
 
   command(:restart, help_available: false) do |event|
-    if event.user.id == 228_290_433_057_292_288 || event.user.id == 348_607_473_546_035_200
+    if event.user.id == 228_290_433_057_292_288
       begin
         event.respond ['Into the ***fuuuutttttuuuuurrrreeee***', 'Please wait...', 'How about n—', 'Can do :thumbsup::skin-tone-1:', 'Pong! Hey, that took... Oh wait, wrong command', 'Ask again at a later ti—'].sample
         exec('bash restart.sh')
