d313b631a472428a9f389111703bc58563076d9a
Chewsterchew <chew@chew.pw>
2018-03-24 17:03:13 -0500

    Fix die command

--- modules/die.rb
+++ modules/die.rb
@@ -3,7 +3,7 @@ module Die
 
   command(:die, help_available: false) do |event|
     if event.user.id == 228_290_433_057_292_288
-      bot.send_message(event.channel.id, 'CahBot is shutting down')
+      event.respond 'CahBot is shutting down'
       exit
     else
       "Hey, you can't do that!"
