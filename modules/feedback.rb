6aea5143c9aa1c0026c3ef26b6b646b795be03e8
Chewsterchew <chew@chew.pw>
2018-03-24 17:06:56 -0500

    Fix feedback command

--- modules/feedback.rb
+++ modules/feedback.rb
@@ -6,13 +6,13 @@ module Feedback
       event.respond "Sorry, you can't send feedback via PM just yet!"
       break
     else
-      event.message.delete
-      bot.send_message(252_239_053_712_392_192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).
-
-  *#{args.join(' ')}*")
-      m = (event.respond 'Radical! Feedback sent.')
-      sleep 5
-      m.delete
+      begin
+        event.message.delete
+      rescue Discordrb::Errors::NoPermission
+        puts 'Unable to delete feedback message, failing silently.'
+      end
+      Bot.send_message(252_239_053_712_392_192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).\n\n*#{args.join(' ')}*")
+      event.send_temporary_message('Radical! Feedback sent.', 5)
     end
   end
 end
