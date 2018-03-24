6a65bd2ffaf388fc58126dc4f767507811a60b77
Chewsterchew <chew@chew.pw>
2018-03-24 17:16:57 -0500

    Let's use chew's certified TM way to load all modules

--- code.rb
+++ code.rb
@@ -7,10 +7,12 @@ Bot = Discordrb::Commands::CommandBot.new token: configatron.token, client_id: 2
 
 Dir["#{File.dirname(__FILE__)}/modules/*.rb"].each { |file| require file }
 
-Modules = [Ban, CahBotServer, Commands, Die, Donate, EBall, Eval, Feedback, FlipFlop, Help, Info, Invite, Leave, Noot, Ping, Restart, Reverse, RNumber, Roll, Say, ServerCount, SetStuff, UserInfo].freeze
-
-Modules.each do |mod|
-  Bot.include! mod
+Dir["#{File.dirname(__FILE__)}/modules/*.rb"].each do |wow|
+  bob = File.readlines(wow) { |line| line.split.map(&:to_s).join }
+  command = bob[0][7..bob[0].length]
+  command.delete!("\n")
+  command = Object.const_get(command)
+  Bot.include! command
   puts "Module #{mod} successfully loaded!"
 end
 
