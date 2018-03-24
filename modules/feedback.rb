13ab1d25b310b86110258c97442be5aefee47f82
Chewsterchew <chew@chew.pw>
2018-03-24 17:23:34 -0500

    [Fixes #11] Allow sending feedback from pm. why not.

--- modules/feedback.rb
+++ modules/feedback.rb
@@ -2,15 +2,13 @@ module Feedback
   extend Discordrb::Commands::CommandContainer
 
   command(:feedback, min_args: 1) do |event, *args|
-    if event.channel.pm? == true
-      event.respond "Sorry, you can't send feedback via PM just yet!"
-      break
-    else
+    unless event.channel.pm? == true
       begin
         event.message.delete
       rescue Discordrb::Errors::NoPermission
         puts 'Unable to delete feedback message, failing silently.'
       end
+    end
       Bot.send_message(252_239_053_712_392_192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).\n\n*#{args.join(' ')}*")
       event.send_temporary_message('Radical! Feedback sent.', 5)
     end
