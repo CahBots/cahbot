90f1e38152159763d1dca255354c82a364d1371a
Chewsterchew <chew@chew.pw>
2018-03-24 17:24:51 -0500

    ...but actually fix the plugin

--- modules/feedback.rb
+++ modules/feedback.rb
@@ -9,8 +9,7 @@ module Feedback
         puts 'Unable to delete feedback message, failing silently.'
       end
     end
-      Bot.send_message(252_239_053_712_392_192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).\n\n*#{args.join(' ')}*")
-      event.send_temporary_message('Radical! Feedback sent.', 5)
-    end
+    Bot.send_message(252_239_053_712_392_192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).\n\n*#{args.join(' ')}*")
+    event.send_temporary_message('Radical! Feedback sent.', 5)
   end
 end
