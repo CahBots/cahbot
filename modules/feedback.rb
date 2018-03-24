3041c3971f2737c6e5150e1235c54ab6a400e29b
Chewsterchew <chew@chew.pw>
2018-03-24 17:26:35 -0500

    ...but actually fix it v2, can i have token for cahbot beta pls

--- modules/feedback.rb
+++ modules/feedback.rb
@@ -2,14 +2,17 @@ module Feedback
   extend Discordrb::Commands::CommandContainer
 
   command(:feedback, min_args: 1) do |event, *args|
-    unless event.channel.pm? == true
+    if event.channel.pm? == true
+      Bot.send_message(252_239_053_712_392_192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of CahBot's pms.\n\n*#{args.join(' ')}*")
+      event.send_temporary_message('Radical! Feedback sent.', 5)
+    else
       begin
         event.message.delete
       rescue Discordrb::Errors::NoPermission
         puts 'Unable to delete feedback message, failing silently.'
       end
+      Bot.send_message(252_239_053_712_392_192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).\n\n*#{args.join(' ')}*")
+      event.send_temporary_message('Radical! Feedback sent.', 5)
     end
-    Bot.send_message(252_239_053_712_392_192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).\n\n*#{args.join(' ')}*")
-    event.send_temporary_message('Radical! Feedback sent.', 5)
   end
 end
