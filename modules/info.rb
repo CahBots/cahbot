a202b28b2345e714eda7c2077e204433a0724ebc
Chewsterchew <chew@chew.pw>
2018-03-30 10:38:37 -0500

    nobody wanted to be commit 200 so here's me fixing ^info instead

--- modules/info.rb
+++ modules/info.rb
@@ -5,7 +5,7 @@ module Info
     event << '***Info About CahBot:***'
     event << ''
     event << '**What is CahBot?** CB is a small Discord bot with loads of potential.'
-    event << '**Who made CahBot?** Cah#5153 coded CahBot, with some help from happyzachariah#6121, tons of help from Chew#6216 & others'
+    event << "**Who made CahBot?** #{Bot.user(228290433057292288).distinct} coded CahBot, with some help from #{Bot.user(133570779576139776).distinct}, tons of help from #{Bot.user(116013677060161545).distinct} & others"
     event << '**Why does CahBot exist?** One day I was bored so I made a Discord bot. End of story kthxbai'
     event << '**Does CahBot have a server or something?** You bet, https://discord.gg/cWmvfmz'
     event << '**u suk a bunnch an u can hardly mak a discord bawt.** Radical, thank you for noticing'
