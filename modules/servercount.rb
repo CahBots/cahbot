e4f7075c65dff7e205384fbd0e4609c2e056b14d
Chewsterchew <chew@chew.pw>
2018-05-02 20:32:33 -0500

    Fix ^servercount whoops lol

--- modules/servercount.rb
+++ modules/servercount.rb
@@ -2,6 +2,6 @@ module ServerCount
   extend Discordrb::Commands::CommandContainer
 
   command(%i[servercount servcount], help_available: false, max_args: 0) do |event|
-    event.respond "CahBot is on **#{bot.servers.count}** servers as of now"
+    event.respond "CahBot is on **#{Bot.servers.count}** servers as of now"
   end
 end
