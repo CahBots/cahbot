b3965b065a86ef1e73c66cb1eb15d138b7d18635
Chewsterchew <chew@chew.pw>
2018-05-05 21:27:29 -0500

    Use chew's discordbotlist library gem :^)

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'configatron'
+gem 'dblruby', '>= 0.6.0'
 gem 'discordrb', '~> 3.2', '>= 3.2.0.1'
--- code.rb
+++ code.rb
@@ -1,9 +1,12 @@
 require 'configatron'
+require 'dblruby'
 require 'discordrb'
 require 'open-uri'
 require_relative 'config.rb'
 
 Bot = Discordrb::Commands::CommandBot.new token: configatron.token, client_id: 249_268_393_817_931_776, prefix: ['<@249268393817931776> ', '^'], ignore_bots: true
+
+DBL = DBLRuby.new(configatron.dbl, '249268393817931776')
 
 Dir["#{File.dirname(__FILE__)}/modules/*.rb"].each { |file| require file }
 
@@ -30,10 +33,12 @@ end
 
 Bot.server_create do |event|
   Bot.send_message(281_280_895_577_489_409, "CahBot just joined `#{event.server.name}` (ID: #{event.server.id}), owned by `#{event.server.owner.distinct}` (ID: #{event.server.owner.id}), the server count is now #{event.bot.servers.count}")
+  DBL.stats.servers = event.bot.servers.count
 end
 
 Bot.server_delete do |event|
   Bot.send_message(281_280_895_577_489_409, "CahBot just left `#{event.server.name}` (ID: #{event.server.id}), the server count is now #{event.bot.servers.count}")
+  DBL.stats.servers = event.bot.servers.count
 end
 
 Bot.run
